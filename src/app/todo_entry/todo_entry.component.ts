b2932096f37442792d076b6dc9442936faab05ed
xkatjusha <katharina.maier.25@web.de>
2022-10-06 12:48:48 +0200

    scss

--- src/app/todo_entry/todo_entry.component.ts
+++ src/app/todo_entry/todo_entry.component.ts
@@ -11,9 +11,11 @@ export class Todo_entryComponent {
   @Input() todo1!: { name: any; list: string[]; };
   todorecords: string;
   deadlinerecords: Date | undefined;
+  dateandhoures : string;
 
   constructor(public elementRef: ElementRef) {
     this.todorecords = "";
+    this.dateandhoures="";
 
 
   }
@@ -29,7 +31,10 @@ export class Todo_entryComponent {
       this.todo1.list.push(this.todorecords);
     }
     else {
-      this.todo1.list.push(this.deadlinerecords + " " + this.todorecords);
+      console.log(this.deadlinerecords.setDate);
+      this.dateandhoures=this.deadlinerecords.setDate+"."+this.deadlinerecords.setMonth+"."+this.deadlinerecords.setFullYear+" "+this.deadlinerecords.setHours+":"+this.deadlinerecords.setMinutes;
+      console.log(this.dateandhoures);
+      this.todo1.list.push(this.dateandhoures + " " + this.todorecords);
     }
 
     console.log(this.todo1.list);
